17a2b372b6f1d36c4e63480ea0ebc1cd6a950af0
natlight <novykovanatalie@gmail.com>
2020-08-25 15:15:42 +0200

    Add map, and buttons in tracker_fragment

--- app/src/main/java/com/example/biketracker/ui/home/TrackerFragment.java
+++ app/src/main/java/com/example/biketracker/ui/home/TrackerFragment.java
@@ -1,9 +1,12 @@
 package com.example.biketracker.ui.home;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,18 +19,40 @@ public class TrackerFragment extends Fragment {
 
     private TrackerViewModel trackerViewModel;
 
+    ImageButton playButton;
+    ImageButton stopButton;
+    boolean isPressed;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
+
         trackerViewModel =
                 ViewModelProviders.of(this).get(TrackerViewModel.class);
         View root = inflater.inflate(R.layout.fragment_tracker, container, false);
-        final TextView textView = root.findViewById(R.id.text_tracker);
-        trackerViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        playButton  = (ImageButton)root.findViewById(R.id.play);
+        stopButton  = (ImageButton)root.findViewById(R.id.stop);
+        Log.e("no"," ### dnovykov PlayButton " + playButton);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                Log.e("no"," ### dnovykov ONCLICK !! WORK");
+                if(!isPressed) {
+                    System.out.println(" ### dnovykov ONCLICK !! WORK 1");
+                    isPressed = true;
+                    playButton.setBackgroundResource(R.drawable.baseline_pause);
+                } else {
+                    Log.e("no"," ### dnovykov ONCLICK !! WORK");
+                    isPressed = false;
+                    playButton.setBackgroundResource(R.drawable.play_arrow);
+                }
             }
         });
+
+
+
         return root;
+
     }
 }
