66ff51a9fd02a8e628953a8c6ce45cc9527d3fdf
natlight <novykovanatalie@gmail.com>
2020-09-13 10:56:31 +0200

    Mode button

--- app/src/main/java/com/example/biketracker/ui/setting/SettingFragment.java
+++ app/src/main/java/com/example/biketracker/ui/setting/SettingFragment.java
@@ -1,36 +1,40 @@
 package com.example.biketracker.ui.setting;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProviders;
-
 import com.example.biketracker.R;
-import com.example.biketracker.ui.setting.SettingViewModel;
-
 public class SettingFragment extends Fragment {
-
+    private Switch switchMode;
     private SettingViewModel settingViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         settingViewModel =
-                ViewModelProviders.of(this).get(SettingViewModel.class);
+                ViewModelProviders.of((FragmentActivity) getContext()).get(SettingViewModel.class);
         View root = inflater.inflate(R.layout.fragment_setting, container, false);
-        final TextView textView = root.findViewById(R.id.text_setting);
-        settingViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        switchMode = root.findViewById(R.id.switchMode);
+        
         return root;
     }
+    public void restartApp() {
+        Intent i = new Intent(getContext(),SettingFragment.class);
+        startActivity(i);
+        onStop();
+    }
+
 }
