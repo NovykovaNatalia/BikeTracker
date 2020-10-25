af7173bb1bb8648ec9c92da2611de35b5666d7ee
natlight <novykovanatalie@gmail.com>
2020-10-25 11:36:46 +0100

    LightTheme

--- app/src/main/java/com/example/biketracker/ui/setting/SettingFragment.java
+++ app/src/main/java/com/example/biketracker/ui/setting/SettingFragment.java
@@ -5,36 +5,63 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProviders;
 import com.example.biketracker.R;
 public class SettingFragment extends Fragment {
+    TextView modeText;
     private Switch switchMode;
-    private SettingViewModel settingViewModel;
+    SharedPreferences sharedPreferences = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        settingViewModel =
-                ViewModelProviders.of((FragmentActivity) getContext()).get(SettingViewModel.class);
         View root = inflater.inflate(R.layout.fragment_setting, container, false);
-
+        modeText = root.findViewById(R.id.textMode);
         switchMode = root.findViewById(R.id.switchMode);
+        switchMode.setChecked(false);
 
+        sharedPreferences = getActivity().getSharedPreferences("night", 0);
+        Boolean booleanValue = sharedPreferences.getBoolean("night_mode"+,true);
+        Log.e("LOG_TAG2", " booleanValue" + booleanValue);
+        if (booleanValue) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            switchMode.setChecked(true);
+        }
+
+        switchMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                    switchMode.setChecked(true);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode",true);
+                    editor.commit();
+                    modeText.setText("Dark");
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//                    switchMode.setChecked(false);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode",false);
+                    editor.commit();
+                    modeText.setText("Light");
+                }
+            }
+        });
         return root;
     }
-    public void restartApp() {
-        Intent i = new Intent(getContext(),SettingFragment.class);
-        startActivity(i);
-        onStop();
-    }
-
 }
