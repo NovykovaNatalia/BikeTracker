0a69c6054ae903287efcd6c5a0af89475150bf0c
Natalia Novykova <natalia@netteam.digital>
2020-11-22 22:18:51 +0100

    add expandableListView for history fragment

--- app/src/main/java/com/example/biketracker/ui/history/HistoryFragment.java
+++ app/src/main/java/com/example/biketracker/ui/history/HistoryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,13 +15,86 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.biketracker.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class HistoryFragment extends Fragment {
+
+    ExpandableListView expandableListView;
+    List<String> listGroup;
+    HashMap<String, List<String>> listItem;
+    MainAdapter adapter;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View fragmentHistory = inflater.inflate(R.layout.fragment_history, container, false);
 
+        expandableListView = fragmentHistory.findViewById(R.id.expandableListView);
+        listGroup = new ArrayList<>();
+        listItem = new HashMap<>();
+        adapter = new MainAdapter(getActivity(), listGroup, listItem);
+        expandableListView.setAdapter(adapter);
+        initListData();
+
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+
+                int index = parent.getFlatListPosition(ExpandableListView
+                        .getPackedPositionForChild(groupPosition, childPosition));
+                parent.setItemChecked(index, true);
+
+                return false;
+            }
+        });
+
+
+
         return fragmentHistory;
+    }
+    private void initListData() {
+        listGroup.add(getString(R.string.group2));
+        listGroup.add(getString(R.string.group3));
+        listGroup.add(getString(R.string.group4));
+        listGroup.add(getString(R.string.group5));
+        listGroup.add(getString(R.string.group6));
+
+        String[] array;
+        List<String> list2 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group2);
+        for (String item : array) {
+            list2.add(item);
+        }
+        List<String> list3 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group3);
+        for (String item : array) {
+            list3.add(item);
+        }
+        List<String> list4 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group4);
+        for (String item : array) {
+            list4.add(item);
+        }
+        List<String> list5 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group5);
+        for (String item : array) {
+            list5.add(item);
+        }
+        List<String> list6 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group6);
+        for (String item : array) {
+            list6.add(item);
+        }
+        listItem.put(listGroup.get(0),list2);
+        listItem.put(listGroup.get(1),list3);
+        listItem.put(listGroup.get(2),list4);
+        listItem.put(listGroup.get(3),list5);
+        listItem.put(listGroup.get(4),list6);
+        adapter.notifyDataSetChanged();
     }
 }
