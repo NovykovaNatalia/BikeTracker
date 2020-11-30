2c69647d029352c4ff40d42ac57cdf32630ba1db
natlight <novykovanatalie@gmail.com>
2020-11-30 19:48:26 +0100

    Implement Firebase + RealtimeDataBase

--- app/src/main/java/com/example/biketracker/MainActivity.java
+++ app/src/main/java/com/example/biketracker/MainActivity.java
@@ -8,15 +8,25 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
     Context ctx;
+    FirebaseDatabase database ;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ctx = this;
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+        myRef.setValue("Hello World");
+
+
 
 //        SharedPreferences sharedPreferences = getSharedPreferences("night", 0);
 //        Boolean isNightMode = sharedPreferences.getBoolean("night_mode",true);
