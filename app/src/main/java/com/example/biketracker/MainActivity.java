6995121139ddbd52370e114490a340180f4537db
natlight <novykovanatalie@gmail.com>
2020-12-11 14:16:20 +0100

    Change applications icon. Implements animation for start activity_main.xml

--- app/src/main/java/com/example/biketracker/MainActivity.java
+++ app/src/main/java/com/example/biketracker/MainActivity.java
@@ -6,26 +6,39 @@ import androidx.appcompat.app.AppCompatDelegate;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     Context ctx;
     FirebaseDatabase database ;
     DatabaseReference myRef;
+    ImageView imageBike;
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ctx = this;
-
+        imageBike = findViewById(R.id.imageBike);
+        imageBike.animate().rotation(360).setDuration(3000);
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("message");
         myRef.setValue("Hello World");
-
+        frameLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable =(AnimationDrawable) frameLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
 
 
 //        SharedPreferences sharedPreferences = getSharedPreferences("night", 0);
@@ -34,7 +47,12 @@ public class MainActivity extends AppCompatActivity {
 //        if (isNightMode) {
 //            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 //        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(ctx, NavigationDrawerActivity.class));
+            }
+        },2000);
 
-        startActivity(new Intent(ctx, NavigationDrawerActivity.class));
     }
 }
